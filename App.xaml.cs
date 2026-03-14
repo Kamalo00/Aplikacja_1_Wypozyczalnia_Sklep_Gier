3586b661bac11c39f51f5a244ce1c5380b5c4ac9
LENOVO <LENOVO@DESKTOP-E2DP3VT>
2026-03-14 16:27:17 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Aplikacja_1_Wypozyczalnia_Sklep_Gier
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
